4b6b223932781a01802f52fbed75304efc2ca09d
Fernando Perez Diez <perezdiezf@gmail.com>
2021-09-29 11:16:13 -0300

    selectFilter bug fixed

--- src/components/selectFilter/selectFilter.jsx
+++ src/components/selectFilter/selectFilter.jsx
@@ -3,34 +3,29 @@ import { useDispatch } from "react-redux";
 import Kernels from "../../Kernels/kernels";
 import Sobel from "../../Sobel/sobel";
 import { addSobel, changeIndex, changeKernel } from "../../redux/actions";
-import { NavDropdown } from "react-bootstrap";
+import { NavDropdown, NavItem, NavLink } from "react-bootstrap";
 
 
 export default function SelectFilter(){
-
     
     const kerSrb = [...Kernels, ...Sobel]
     const dispatch = useDispatch()
-    function clicked(i){        
+    function clicked(i){   
+        console.log(i)     
         dispatch(addSobel({}))
         dispatch(changeIndex(i))
         if (i < Kernels.length) dispatch(changeKernel(Kernels[i]))
         else dispatch(addSobel(Sobel[i - Kernels.length]))
     }
-    
 
- 
     return ( 
-        <div className="col-4">
-           
-            <NavDropdown onChange={(e) => clicked(e.target.value)} title="Select a filter" id="basic-nav-dropdown">
-           
-            { kerSrb?.map((kernel, idx) => {   
-                return  <NavDropdown.Item key={idx} value={idx} >{kernel.name}</NavDropdown.Item>
-            
-            
-            })}
-        </NavDropdown>
+        <div>
+            <NavDropdown onSelect={clicked} title="Select a filter" id="basic-nav-dropdown">           
+                { kerSrb?.map((kernel, idx) => {   
+                return  <NavItem key={idx} >
+                            <NavLink eventKey={idx} title={kernel.name}>{kernel.name}</NavLink>
+                        </NavItem> })}            
+                </NavDropdown>
         </div>
     )
 }
