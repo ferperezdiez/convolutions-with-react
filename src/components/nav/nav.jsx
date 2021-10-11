179cc553074e512ba93111f9a337a16753cbbc60
Fernando Perez Diez <perezdiezf@gmail.com>
2021-10-11 13:17:42 -0300

    x

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -12,7 +12,7 @@ export default function Nav(){
     return (
         
     <Navbar bg="light" expand="lg" >
-        <Container className="d-inline-flex p-2 bd-highlight">
+        <Container className=" p-2 bd-highlight">
             <Navbar.Toggle aria-controls="row row-cols-lg-5 g-2 g-lg-3 basic-navbar-nav" />
                 <Navbar.Collapse className="navbar-collapse collapse collap" >
                             <ul className="ulNav">
